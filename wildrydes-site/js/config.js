338c11076ed07f18d7243f526d544474fadb6626
Alice Jiang <97613431+Alice-2096@users.noreply.github.com>
2022-11-08 10:43:15 -0500

    Add files via upload

---
+++ wildrydes-site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: 'us-east-1_OLFs9JOTk', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '7d4dq7oufav2ooh0evkra9gbb8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'us-east-1', // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: 'https://bs5huez81d.execute-api.us-east-1.amazonaws.com/prod',
+  },
+};
